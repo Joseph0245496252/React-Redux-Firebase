f3fb14e3a7c5c95056ef02cd8975ef621e7b3fad
Joseph Asante <freecoding1@gmail.com>
2020-02-26 16:56:44 +0000

    rendered from init to redux with sucess

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,51 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class SignUp extends Component {
-    staate = {
-        email: '',
-        password: '',
-        firstName: '',
-        listName: ''
-
-    }
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    } 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.state)
-    } 
+  state = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-        <h5 className="grey-text text-darken-3">Sign Up</h5>
-        <div className="input-field">
-            <label htmlform="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
-        </div>
-        <div className="input-field">
-            <label htmlform="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
-        </div>
-        <div className="input-field">
-            <label htmlform="firstName">First Name</label>
-            <input type="text" id="firstName" onChange={this.handleChange} />
-        </div>
-        <div className="input-field">
-            <label htmlform="lastName">Last Name</label>
-            <input type="text" id="lastName" onChange={this.handleChange} />
-        </div>
-        <div className="input-field">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id='firstName' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id='lastName' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
-        </div>
+          </div>
         </form>
       </div>
-    );
+    )
   }
 }
 
-
-export default SignUp;
+export default SignUp
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,16 +2,14 @@ import React from 'react'
 import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
-    return (
-        <div className="Project-list section">
-
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
-
-        </div>
-    )
+  return (
+    <div className="project-list section">  
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+    </div>
+  )
 }
 
-export default ProjectList;
+export default ProjectList
